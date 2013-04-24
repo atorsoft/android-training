ad5d0d644d684d266574a2cf8a998c15eee0ce2e
atorsoft <atorsoft@gmail.com>
2013-04-24 13:35:37 +0200

    Project 6 (version 2): Map (Google Maps Android API v2)

---
+++ P6_Mapa_ApiV2/src/atorsoft/mapa/Mapa.java
@@ -0,0 +1,13 @@
+package atorsoft.mapa;
+
+import android.os.Bundle;
+
+public class Mapa extends android.support.v4.app.FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_mapa);
+	}
+
+}
